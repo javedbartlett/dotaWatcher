f68c762608264b8483f142e3cf98147432817d8d
Javed Bartlett <javedbartlett@gmail.com>
2020-01-09 22:57:01 -0800

    minor syntax changes

--- helpers/steam.js
+++ helpers/steam.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const rp = require('request-promise-native');
-const KEY = require('./config.js').KEY;
+const KEY = process.env.TOKEN || require('./config.js').KEY;
 
 const getGames = async () => {
   return await rp(`https://api.steampowered.com/IDOTA2Match_570/GetTopLiveGame/v1?key=${KEY}&partner=0`)
