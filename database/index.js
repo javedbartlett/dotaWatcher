8206e16df0190a4eb0d5c68f96f5b4661e43bb89
Javed Bartlett <javedbartlett@gmail.com>
2020-01-09 23:40:45 -0800

    removed useUnifiedTopology flag

--- database/index.js
+++ database/index.js
@@ -3,7 +3,6 @@ let mongoUri = 'mongodb://javedb:porky123@ds361768.mlab.com:61768/heroku_fqp98kd
 
 
 const db = mongoose.connect(mongoUri, {
-  useUnifiedTopology: false,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
