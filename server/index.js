e282504f633f1c7f41064f866fd4fe381c9bcc0b
Javed Bartlett <javedbartlett@gmail.com>
2020-01-10 17:16:13 -0800

    works on localhost and heroku

--- server/index.js
+++ server/index.js
@@ -104,7 +104,7 @@ app.get('/api/update', async (req, res) => {
 const update = async () => {
   console.log('updating')
   // await removeAll()
-  await rp(`http://localhost:${process.env.PORT}/api/update`)
+  await rp(`http://localhost:${process.env.PORT||'3222'}/api/update`)
   .catch(err => console.log('error from rp /api/update'));
 }
 setInterval(removeAll, 300000);
