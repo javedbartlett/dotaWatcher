cf414dcb8d6422121c5ddc08b3440c2f1c936296
Javed Bartlett <javedbartlett@gmail.com>
2020-01-11 18:59:17 -0800

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <div>
+    <div className="headerContainer">
   <h1 className="title">Dota Player Tracker</h1>
   <h5 className="notice">*Games auto-update (no need to refresh page)</h5>
+  <div className="liveGamesTitleContainer">
+  </div>
   </div>
   )
 }
