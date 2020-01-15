d6d6f9ef12f4b8e26c4759c736996b01d39e23e9
Javed Bartlett <javedbartlett@gmail.com>
2020-01-14 20:20:05 -0600

    header message change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(props) {
   return (
     <div className="headerContainer">
   <h1 className="title"><Link to="/">Dota Player Tracker</Link></h1>
-  <h5 className="notice">*Games auto-update (no need to refresh page)</h5>
+  <h5 className="notice">*Games auto-update</h5>
   <div className="liveGamesTitleContainer">
   </div>
   </div>
